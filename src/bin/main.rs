0fc24c59007aa20b113907b39296e32bedf38723
Santiago Narvaez <santiagonar1@gmail.com>
2022-05-25 16:23:47 +0200

    Include binary
    The binary will act as an example of how to use our API

---
+++ src/bin/main.rs
@@ -0,0 +1,8 @@
+use life::game::Game;
+
+fn main() {
+
+        let coord_alive_cells = [(1, 2), (2, 1), (2, 2), (2, 4)];
+        let mut game = Game::new(10, &coord_alive_cells);
+        game.next();
+}
