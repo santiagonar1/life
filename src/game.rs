e17904c4d69cdd72923a160cce9a65be97a59d58
Santiago Narvaez <santiagonar1@gmail.com>
2022-05-31 17:11:23 +0200

    Add function to create a game from a file
    We now have the option to indicate the initial state of the game via a
    simple file.

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,8 @@
 use crate::field::Field;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const ALIVE_SYMBOL: &str = "x";
 
 pub struct Game {
     field: Field,
@@ -9,6 +13,32 @@ impl Game {
         Game {
             field: Field::new(size, coord_alive_cells),
         }
+    }
+
+    pub fn new_from_file(filepath: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let file = match File::open(filepath) {
+            Err(why) => {
+                eprintln!("The file {} does not exist", filepath);
+                return Err(Box::new(why));
+            }
+            Ok(file) => file,
+        };
+
+        let reader = BufReader::new(file);
+
+        let mut coord_alive_cells = Vec::new();
+        let mut size = 0;
+
+        for (row, line) in reader.lines().enumerate() {
+            size = row + 1;
+            for (column, symbol) in line.unwrap().split(' ').enumerate() {
+                if symbol == ALIVE_SYMBOL {
+                    coord_alive_cells.push((row, column));
+                }
+            }
+        }
+
+        Ok(Game::new(size, &coord_alive_cells))
     }
 
     pub fn next(&mut self) {
@@ -27,5 +57,12 @@ mod tests {
     #[test]
     fn can_create_game() {
         let _game = Game::new(10, &[]);
+    }
+
+    #[test]
+    fn fails_when_file_does_not_exist() {
+        if let Ok(_game) = Game::new_from_file("does_not_exist.txt") {
+            panic! {"It did not return an error when file does not exist"};
+        }
     }
 }
--- tests/oscillators.rs
+++ tests/oscillators.rs
@@ -14,3 +14,18 @@ fn blinker() {
     let expected_coord_alive_cells: Vec<(usize, usize)> = vec![(2, 1), (2, 2), (2, 3)];
     assert_eq!(expected_coord_alive_cells, game.coord_alive_cells());
 }
+
+#[test]
+fn blinker_from_file() {
+    // See: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+    let expected_coord_alive_cells: Vec<(usize, usize)> = vec![(1, 2), (2, 2), (3, 2)];
+    let mut game: Game = Game::new_from_file("tests/blinker.txt").unwrap();
+
+    assert_eq!(expected_coord_alive_cells, game.coord_alive_cells());
+
+    game.next();
+
+    let expected_coord_alive_cells: Vec<(usize, usize)> = vec![(2, 1), (2, 2), (2, 3)];
+    assert_eq!(expected_coord_alive_cells, game.coord_alive_cells());
+}
