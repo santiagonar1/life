dcb7a9bf8ba65a8e2d9c5d443fac781337ec6724
Santiago Narvaez <santiagonar1@gmail.com>
2022-06-01 12:37:06 +0200

    Game: Add field to store max number of iterations
    We will use this information in a future commit to create a play
    function, such that the user does not have to execute next each time

--- src/game.rs
+++ src/game.rs
@@ -3,15 +3,18 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 const ALIVE_SYMBOL: &str = "x";
+const DEFAULT_NUM_ITERATIONS: u32 = 20;
 
 pub struct Game {
     field: Field,
+    max_num_iterations: u32,
 }
 
 impl Game {
     pub fn new(size: usize, coord_alive_cells: &[(usize, usize)]) -> Self {
         Game {
             field: Field::new(size, coord_alive_cells),
+            max_num_iterations: DEFAULT_NUM_ITERATIONS,
         }
     }
 
@@ -48,6 +51,10 @@ impl Game {
     pub fn coord_alive_cells(&self) -> Vec<(usize, usize)> {
         self.field.coord_alive_cells()
     }
+
+    pub fn set_max_num_iterations(&mut self, max_num_iterations: u32) {
+        self.max_num_iterations = max_num_iterations;
+    }
 }
 
 #[cfg(test)]
@@ -64,5 +71,15 @@ mod tests {
         if let Ok(_game) = Game::new_from_file("does_not_exist.txt") {
             panic! {"It did not return an error when file does not exist"};
         }
+    }
+
+    #[test]
+    fn can_set_max_num_iterations() {
+        let mut game = Game::new(10, &[]);
+        assert_eq!(game.max_num_iterations, DEFAULT_NUM_ITERATIONS);
+
+        let expected = DEFAULT_NUM_ITERATIONS + 10;
+        game.set_max_num_iterations(expected);
+        assert_eq!(game.max_num_iterations, expected);
     }
 }
